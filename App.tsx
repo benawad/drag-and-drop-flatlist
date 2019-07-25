91bf698ddb1115dc3aceb305244e954c4006b9f1
Ben Awad <benawad97@gmail.com>
2019-07-25 18:08:36 -0500

    reordering rows

--- App.tsx
+++ App.tsx
@@ -18,11 +18,33 @@ function getRandomColor() {
   return color;
 }
 
+function immutableMove(arr, from, to) {
+  return arr.reduce((prev, current, idx, self) => {
+    if (from === to) {
+      prev.push(current);
+    }
+    if (idx === from) {
+      return prev;
+    }
+    if (from < to) {
+      prev.push(current);
+    }
+    if (idx === to) {
+      prev.push(self[from]);
+    }
+    if (from > to) {
+      prev.push(current);
+    }
+    return prev;
+  }, []);
+}
+
 const colorMap = {};
 
 export default class App extends React.Component {
   state = {
     dragging: false,
+    draggingIdx: -1,
     data: Array.from(Array(200), (_, i) => {
       colorMap[i] = getRandomColor();
       return i;
@@ -31,6 +53,12 @@ export default class App extends React.Component {
 
   _panResponder: PanResponderInstance;
   point = new Animated.ValueXY();
+  currentY = 0;
+  scrollOffset = 0;
+  flatlistTopOffset = 0;
+  rowHeight = 0;
+  currentIdx = -1;
+  active = false;
 
   constructor(props) {
     super(props);
@@ -46,10 +74,18 @@ export default class App extends React.Component {
         // The gesture has started. Show visual feedback so the user knows
         // what is happening!
         // gestureState.d{x,y} will be set to zero now
-        console.log(gestureState.y0);
-        this.setState({ dragging: true });
+        this.currentIdx = this.yToIndex(gestureState.y0);
+        this.currentY = gestureState.y0;
+        Animated.event([{ y: this.point.y }])({
+          y: gestureState.y0 - this.rowHeight / 2
+        });
+        this.active = true;
+        this.setState({ dragging: true, draggingIdx: this.currentIdx }, () => {
+          this.animateList();
+        });
       },
       onPanResponderMove: (evt, gestureState) => {
+        this.currentY = gestureState.moveY;
         Animated.event([{ y: this.point.y }])({ y: gestureState.moveY });
         // The most recent move distance is gestureState.move{X,Y}
         // The accumulated gesture distance since becoming responder is
@@ -59,10 +95,12 @@ export default class App extends React.Component {
       onPanResponderRelease: (evt, gestureState) => {
         // The user has released all touches while this view is the
         // responder. This typically means a gesture has succeeded
+        this.reset();
       },
       onPanResponderTerminate: (evt, gestureState) => {
         // Another component has become the responder, so this gesture
         // should be cancelled
+        this.reset();
       },
       onShouldBlockNativeResponder: (evt, gestureState) => {
         // Returns whether this component should block native components from becoming the JS
@@ -72,18 +110,63 @@ export default class App extends React.Component {
     });
   }
 
-  render() {
-    const { data, dragging } = this.state;
+  animateList = () => {
+    if (!this.active) {
+      return;
+    }
 
-    const renderItem = ({ item }) => (
+    requestAnimationFrame(() => {
+      // check y value see if we need to reorder
+      const newIdx = this.yToIndex(this.currentY);
+      if (this.currentIdx !== newIdx) {
+        this.setState({
+          data: immutableMove(this.state.data, this.currentIdx, newIdx),
+          draggingIdx: newIdx
+        });
+        this.currentIdx = newIdx;
+      }
+
+      this.animateList();
+    });
+  };
+
+  yToIndex = (y: number) => {
+    const value = Math.floor(
+      (this.scrollOffset + y - this.flatlistTopOffset) / this.rowHeight
+    );
+
+    if (value < 0) {
+      return 0;
+    }
+
+    if (value > this.state.data.length - 1) {
+      return this.state.data.length - 1;
+    }
+
+    return value;
+  };
+
+  reset = () => {
+    this.active = false;
+    this.setState({ dragging: false, draggingIdx: -1 });
+  };
+
+  render() {
+    const { data, dragging, draggingIdx } = this.state;
+
+    const renderItem = ({ item, index }, noPanResponder = false) => (
       <View
+        onLayout={e => {
+          this.rowHeight = e.nativeEvent.layout.height;
+        }}
         style={{
           padding: 16,
           backgroundColor: colorMap[item],
-          flexDirection: "row"
+          flexDirection: "row",
+          opacity: draggingIdx === index ? 0 : 1
         }}
       >
-        <View {...this._panResponder.panHandlers}>
+        <View {...(noPanResponder ? {} : this._panResponder.panHandlers)}>
           <Text style={{ fontSize: 28 }}>@</Text>
         </View>
         <Text style={{ fontSize: 22, textAlign: "center", flex: 1 }}>
@@ -94,21 +177,31 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={{
-            backgroundColor: "black",
-            zIndex: 2,
-            width: "100%",
-            top: this.point.getLayout().top
-          }}
-        >
-          {renderItem({ item: 3 })}
-        </Animated.View>
+        {dragging && (
+          <Animated.View
+            style={{
+              position: "absolute",
+              backgroundColor: "black",
+              zIndex: 2,
+              width: "100%",
+              top: this.point.getLayout().top
+            }}
+          >
+            {renderItem({ item: data[draggingIdx], index: -1 }, true)}
+          </Animated.View>
+        )}
         <FlatList
           scrollEnabled={!dragging}
           style={{ width: "100%" }}
           data={data}
           renderItem={renderItem}
+          onScroll={e => {
+            this.scrollOffset = e.nativeEvent.contentOffset.y;
+          }}
+          onLayout={e => {
+            this.flatlistTopOffset = e.nativeEvent.layout.y;
+          }}
+          scrollEventThrottle={16}
           keyExtractor={item => "" + item}
         />
       </View>
